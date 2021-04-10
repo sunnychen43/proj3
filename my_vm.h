ea2807d3d106a8e1e5a96d97107bd4d38eb6799d
Sunny <sunnychen2000@gmail.com>
2021-04-09 22:32:23 -0400

    a

--- my_vm.h
+++ my_vm.h
@@ -37,7 +37,7 @@ typedef struct block {
 
 
 void set_physical_mem();
-pte_t* translate(pde_t *pgdir, void *va);
+void *translate(pde_t *pgdir, void *va);
 int page_map(pde_t *pgdir, void *va, void* pa);
 bool check_in_tlb(void *va);
 void put_in_tlb(void *va, void *pa);
